3646d608057a2241d5187e872a064d58f9bace6e
Blanco <blajuano@amazon.com>
2024-01-17 19:45:32 -0600

    arreglando el code in evt parameter

--- src/Board.js
+++ src/Board.js
@@ -32,31 +32,40 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
   function createBoard() {
-    let temp =[]; //
+    let temp = []; //
     let initialBoard = [];
     // TODO: create array-of-arrays of true/false values
-    temp = Array.from({length: nrows}).map(
-      row => Array.from({length: ncols}))
-    initialBoard = temp.map(n =>  n.map(m => Math.random() > chanceLightStartsOn ? 1 : 0))
+    temp = Array.from({ length: nrows }).map((row) =>
+      Array.from({ length: ncols })
+    );
+    initialBoard = temp.map((n) =>
+      n.map((m) => (Math.random() > chanceLightStartsOn ? 1 : 0))
+    );
     return initialBoard;
   }
   //JD function to find cell around return a array
-  function findCellsAround(x,y) {
-      return [ [x,y],[(x -1),y],[x,(y-1)],[x,(y+1)],[(x+1),y] ]
+  function findCellsAround(x, y) {
+    return [
+      [x, y],
+      [x - 1, y],
+      [x, y - 1],
+      [x, y + 1],
+      [x + 1, y],
+    ];
   }
   //end
   function hasWon(board) {
     // TODO: check the board in state to determine whether the player has won.
     //stand by to change the array -name  acording with the state parameter
     //
-    return !board.map( f => f.some(x => x === 0)).some(x => x === true)//return true if it won
+    return !board.map((f) => f.some((x) => x === 0)).some((x) => x === true); //return true if it won
   }
 
   function flipCellsAround(coord) {
-    setBoard(oldBoard => {
+    setBoard((oldBoard) => {
       const [x, y] = coord.split("-").map(Number);
 
-     /*  const flipCell = (x, y, boardCopy) => {
+      /*  const flipCell = (x, y, boardCopy) => {
         // if this coord is actually on board, flip it
           
         if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
@@ -64,35 +73,33 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
           boardCopy[x][y] = boardCopy[x][y] === 0 ?  1:0;
           console.log("x=" + x + " Y=" + y + "ini =" + ini + "end="+ boardCopy[x][y] );
         } */
-        // ? flipCell shuld be defiane as le or const ???
-        let flipCells = (cellToChange,boardCopy)=>{
-          console.log('cellToChange: ', cellToChange);
-          
-          cellToChange.map(cell => {
-            let x = cell[0];
-            let y = cell[1];
-            if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
-              let ini = boardCopy[x][y];//where to init this variable ?//temp  to print
-              boardCopy[x][y] = boardCopy[x][y] === 0 ?  1:0;
-              //console.log("x=" + x + " Y=" + y + "ini =" + ini + "end="+ boardCopy[x][y] );
-            } 
-          })};
-        
-      
+      // ? flipCell shuld be defiane as le or const ???
+      let flipCells = (cellToChange, boardCopy) => {
+        console.log("cellToChange: ", cellToChange);
+
+        cellToChange.map((cell) => {
+          let x = cell[0];
+          let y = cell[1];
+          if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
+            let ini = boardCopy[x][y]; //where to init this variable ?//temp  to print
+            boardCopy[x][y] = boardCopy[x][y] === 0 ? 1 : 0;
+            //console.log("x=" + x + " Y=" + y + "ini =" + ini + "end="+ boardCopy[x][y] );
+          }
+        });
+      };
 
       // TODO: Make a (deep) copy of the oldBoard
       //const deepCopy = structuredClone(oldBoard);
-      const deepCopy = oldBoard.map(row => [...row]);
+      const deepCopy = oldBoard.map((row) => [...row]);
       // TODO: in the copy, flip this cell and the cells around it
       /* const cellsAround = findCellsAround(x,y);
       cellsAround.map(i => console.log("x,y ",i[0] + " " + i[1]));
       const a = cellsAround.map(i => flipCell(i[0],i[1], deepCopy));
       return deepCopy; */
-      flipCells(findCellsAround(x,y),deepCopy);
+      flipCells(findCellsAround(x, y), deepCopy);
       //not sure what is the right parameter deepCopy/oldboard -- why we had to create another board
       // TODO: return the copy
       return deepCopy;
-
     });
   }
 
@@ -101,37 +108,38 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
     return <div>You Win!</div>;
   }
   // TODO
-  function tempfun (evt) {
-    console.log("evt ",evt.target.id);
+  function tempfun(evt) {
+    console.log("evt ", evt.target.id);
     flipCellsAround(evt.target.id);
   }
   // make table board
   let tableBoard = [];
   let cellData;
-  for (let x=0;  x< ncols; x++) {
-    let row =[ ];
-    for (let y=0 ; y< ncols; y++){
-      cellData = {"x": x, "y" : y , "islit": board[x][y]}; 
+  for (let x = 0; x < ncols; x++) {
+    let row = [];
+    for (let y = 0; y < ncols; y++) {
+      cellData = { x: x, y: y, islit: board[x][y] };
       //console.log('cellData', cellData);
       row.push(
-        <Cell 
+        <Cell
           key={cellData.x + "-" + cellData.y}
-          data = {cellData}
-          lit = {cellData.islit}
-          flipCellsAroundMe={(evt) => tempfun(event)}
-          />
-          );
-          //flipCellsAroundMe={(evt) => flipCellsAround(EventTarget.cellData.x + "-" + cellData.y)} />
-          //mycellsAround = {findCellsAround(x,y)}
-          //
-      
+          data={cellData}
+          lit={cellData.islit}
+          flipCellsAroundMe={(evt) => tempfun(evt)}
+        />
+      );
+      //flipCellsAroundMe={(evt) => flipCellsAround(EventTarget.cellData.x + "-" + cellData.y)} />
+      //mycellsAround = {findCellsAround(x,y)}
+      //
     }
-    tableBoard.push(<tr key = {x}>{row}</tr>);
+    tableBoard.push(<tr key={x}>{row}</tr>);
   }
   // TODO
-  return <table className="Board"> 
-    <tbody>{tableBoard}</tbody>
-  </table>
+  return (
+    <table className="Board">
+      <tbody>{tableBoard}</tbody>
+    </table>
+  );
 }
 
 export default Board;
